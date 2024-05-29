6b46b276a20a79aeb4be35497ecff78d96b1cfdd
vugn <gustiranda3013@gmail.com>
2024-05-29 06:14:19 +0000

    🚧 Auth Feature

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -16,4 +16,47 @@ class Login extends CI_Controller
 
         $this->load->view('login/index');
     }
+
+    public function doLogin()
+    {
+        $user = $this->input->post('email');
+        $pw = $this->input->post('password');
+        $user = $this->db->get_where('user', ['email' => $user])->row_array();
+        if ($user) {
+            if (password_verify($pw, $user['password'])) {
+                $data = [
+                    'id' => $user['id'],
+                    'email' => $user['email'],
+                    'password' => $user['password'],
+                    'role' => $user['role'],
+                ];
+                $userId = $user['id'];
+                $this->session->set_userdata($data);
+                if ($user['role'] == "PEMILIK") {
+                    $this->_updateLastLogin($userId);
+                    redirect('menu');
+                } elseif ($user['role'] == "ADMIN") {
+                    $this->_updateLastLogin($userId);
+                    redirect('user');
+                } elseif ($user['role'] == "KASIR") {
+                    $this->_updateLastLogin($userId);
+                    redirect('kasir');
+                } else {
+                    redirect('login');
+                }
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger fw-bold" role="alert">Password Salah!</div>');
+                redirect('/');
+            }
+        } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-warning fw-bold" role="alert">Akun Tidak Terdaftar!</div>');
+            redirect('/');
+            // ☝️
+        }
+    }
+    private function _updateLastLogin($userId)
+    {
+        $sql = "UPDATE user SET last_login=now() WHERE id=$userId";
+        $this->db->query($sql);
+    }
 }
--- application/views/login/index.php
+++ application/views/login/index.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
@@ -18,32 +20,35 @@
                             <div class="mb-3 fs-3 fw-bold text-light">Login</div>
                             <hr class="text-light">
                             <div class="divider"></div>
+
+                            <?php echo $this->session->flashdata('message'); ?>
+
                             <form action="<?= base_url('login/doLogin') ?>" method="POST" class="text-light">
                                 <div class="mb-3">
                                     <label for="" class="form-label">Email address</label>
-                                    <input type="email" class="form-control" id="" aria-describedby="emailHelp">
+                                    <input name="email" type="email" class="form-control" id="inputEmail" placeholder="name@example.com" required>
                                     <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
                                 </div>
                                 <div class="mb-3">
                                     <label for="" class="form-label ">Password</label>
-                                    <input type="password" class="form-control" id="">
+                                    <input name="password" type="password" class="form-control" id="inputPassword" placeholder="Password" required>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="mb-3 form-check">
-                                            <input type="checkbox" class="form-check-input" id="">
-                                            <label class="form-check-label " for="">Remmember Me</label>
+                                            <input type="checkbox" class="form-check-input" id="inputRememberPassword">
+                                            <label class="form-check-label " for="">Remember Me</label>
                                         </div>
                                     </div>
                                     <div class="col-md-6 d-flex justify-content-end">
-                                        <a href="">Forgot Password?</a>
+                                        <a href="password.html">Forgot Password?</a>
                                     </div>
                                 </div>
                                 <div class="row">
-
-                                    <button type="submit" class="btn btn-primary fw-bold">Login</button>
+                                    <input type="submit" class="btn btn-primary fw-bold" value="Login">
                                 </div>
                             </form>
+
                         </div>
                     </div>
                 </div>
@@ -52,6 +57,7 @@
         </div>
     </div>
 
+    <!-- <script src="<?= base_url('assets/js/scripts.js') ?>"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
